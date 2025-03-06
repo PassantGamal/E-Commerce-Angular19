8a8ce2ab63bb618487747be68269f4a7b7373da1
PassantGamal <basantgamal070@gmail.com>
2025-03-06 06:38:08 +0200

    test

--- src/app/core/interceptors/errors/error.interceptor.ts
+++ src/app/core/interceptors/errors/error.interceptor.ts
@@ -1,26 +1,13 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const toastrService = inject(ToastrService);
-  const router = inject(Router);
-
-  // Check if the request is going to a protected API endpoint
-  const isProtectedApi = req.url.includes(`${environment.baseUrl}`); // Change '/api/' to match your API base URL
-
   return next(req).pipe(
     catchError((err) => {
-      if (isProtectedApi && err.status === 401) {
-        toastrService.error(
-          'You are not logged in. Please login to get access.',
-          'FreshCart'
-        );
-        router.navigate(['/login']);
-      }
+      toastrService.error(err.error.message, 'FreshCart');
       return throwError(() => err);
     })
   );
